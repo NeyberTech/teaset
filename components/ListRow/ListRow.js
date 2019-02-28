84a9e480f76d599c8a0d6f711f40b929c68e84ba
linyuanze <1577932922@qq.com>
2019-02-28 11:22:35 +0800

    fix titleMultiLine propType

--- components/ListRow/ListRow.js
+++ components/ListRow/ListRow.js
@@ -18,7 +18,7 @@ export default class ListRow extends Component {
     title: PropTypes.oneOfType([PropTypes.element, PropTypes.string, PropTypes.number]),
     detail: PropTypes.oneOfType([PropTypes.element, PropTypes.string, PropTypes.number]),
     titleStyle: Text.propTypes.style,
-    titleMultiLine: Text.propTypes.bool,
+    titleMultiLine: PropTypes.bool,
     detailStyle: Text.propTypes.style,
     detailMultiLine: PropTypes.bool, //是否支持多行内容
     icon: PropTypes.oneOfType([PropTypes.element, PropTypes.shape({uri: PropTypes.string}), PropTypes.number]),
@@ -36,6 +36,7 @@ export default class ListRow extends Component {
     topSeparator: 'none',
     bottomSeparator: 'indent',
     titlePlace: 'left',
+    titleMultiLine: false
   };
 
   static SwipeActionButton = SwipeActionButton;
